d6da4765cd7603085de6305d40180a4fd61312a7
njenkins <web@nathanjenkins.com.au>
2016-11-12 19:53:59 +1100

    Update return text

--- Christmas.py
+++ Christmas.py
@@ -8,7 +8,7 @@ def handle(text, mic, profile):
     d1 = datetime.datetime.today()
     d2 = datetime.datetime(2016,12,25)
     difference = (d2-d1).days
-    mic.say("It's " + str(difference) + " until christmas");
+    mic.say("It's " + str(difference) + " days until christmas");
 
 
 def isValid(text):
