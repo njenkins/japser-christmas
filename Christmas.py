d1f2ad0fe74b588e57023d24989f7cd4821c3a85
nathan jenkins <web@nathanjenkins.com.au>
2016-11-12 18:58:42 +1100

    Initial commit

---
+++ Christmas.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8-*-
+import re
+import datetime
+
+WORDS = ["HOW" "LONG" "DAYS" "UNTIL" "CHRISTMAS" "SANTA" "WHEN" "IS"];
+
+def handle(text, mic, profile):
+    d1 = datetime.datetime.today()
+    d2 = datetime.datetime(2016,12,25)
+    difference = (d2-d1).days
+    mic.say("It's only " + str(difference) + " until christmas");
+
+
+def isValid(text):
+    return bool(re.search(r'\b((days|how long)(until|til)|when is)|(christmas|santa)\b', text, re.IGNORECASE))
---
+++ README.md
@@ -0,0 +1,8 @@
+# Japser Christmas Module
+
+This Jasper module returns the number of days until Christmas.
+
+Usage example
+
+* **You:** Japser. How many days until Christmas?
+* **Jasper:** 15 days until Christmas.
